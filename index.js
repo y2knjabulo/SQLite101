ad75be4b12199795e793167796dde18b3fefa1f2
y2knjabulo <y2knjabulo00gmail.com>
2023-08-14 12:55:22 +0200

    three

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
-// import * as sqlite from "sqlite";
-// import sqlite3 from "sqlite3";
+import * as sqlite from "sqlite";
+import sqlite3 from "sqlite3";
 import express from 'express'
-import {addGreeting, deleteGreeting, getGreetings, updateGreeting} from './db.js'
+import {getGreetings, addGreetingS} from './db.js'
 
 const app = express();
 app.use(express.json());
@@ -14,25 +14,40 @@ const PORT = process.env.PORT || 4001;
 // http://localhost:4001/api/greetings
 //get all the greetings 
 
-app.get('/api/greetings', async (req, res) => {
-    const greetings = await getGreetings();
-    console.log(greetings);
+app.get('/api/greetings', async (reg, res) => {
+    console.log('start')
     res.json({
-        greetings
+        greetings : []
     })
 
 });
 
 // create a route to add a greeting 
-app.post('/api/greeting', async (req, res) => {
-    const greetings = req.body.greeting;
-    const language = req.body.language;
+// app.post('/api/greeting', async (req, res) => {
+//     const greetings = req.body.greeting;
+//     const language = req.body.language;
     
-    req.json({
-        greetings,
-        language
-    })
+//     req.json({
+//         greetings,
+//         language
+//     })
 
-});
+// });
 
 app.listen(PORT, () => console.log`started on port: ${PORT}`)
+const db = await sqlite.open({
+    filename: './101.db',
+    driver: sqlite3.Database
+});
+// create a function that returns all the greetings from db
+async function getGreetings() {
+
+    const result = await db.all('select * from greetings');
+    return result;
+}
+
+console.log("===============")
+
+await addGreeting('Venda', 'Ndaa');
+const result = await getGreetings()
+console.log(result)
